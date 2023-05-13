72333694302cf0c636b9b4ca01cb4fedd91a4dd7
Ayivi <kafuigalley7@gmail.com>
2023-05-13 09:45:01 -0700

    initial2

---
+++ pycodestyle_compliant.py
@@ -0,0 +1,17 @@
+def calculate_area(radius):
+    """Calculate the area of a circle."""
+    pi = 3.14159
+    area = pi * radius ** 2
+    return area
+
+
+def main():
+    """Main function that calculates the area of a circle."""
+    radius = 5
+    area = calculate_area(radius)
+    print(f"The area of a circle with radius {radius} is {area:.2f}")
+
+
+if __name__ == "__main__":
+    main()
+
